02a5861d12ca70a80b8e11c85ea6bcc22f8ad4ad
Maggie Liang <junyu310024@gmail.com>
2025-05-25 21:11:40 -0500

    Convert travel templete from static to dynamic json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync(__dirname + '/../data/trips.json', 'utf8'));
+
+
+/* Get traveler view */ 
 const travel = (req, res) => {
   const pageTitle = 'Travlr Getaways - Travel';
-  res.render('travel', { title: pageTitle });
+  res.render('travel', { title: pageTitle, trips });
 };
 
 module.exports = {
